7d897f50377b8498d78b46848713bb21812f313d
akrasny <alexander.a.krasny@gmail.com>
2015-05-23 15:36:58 +0300

    in progress

---
+++ WebAPIExp/Controllers/SomeController.cs
@@ -0,0 +1,45 @@
+﻿using DataLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIExp.Controllers
+{
+    public class SomeController : ApiController
+    {
+        public SomeController(IMySvcRepository r)
+        {
+
+        }
+
+        // GET: api/Some
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Some/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Some
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Some/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Some/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ WebAPIExp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIExp.WebApiApplication" Language="C#" %>
