75af25a16f849a0ab5ad8b8abf31bbfb53410d5f
akrasny <alexander.a.krasny@gmail.com>
2015-05-23 14:30:03 +0300

    Initial commit

---
+++ DataLayer/Implementation/MySvcRepository.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Entities;
+using DataLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Implementation
+{
+    public class MySvcRepository : IMySvcRepository
+    {
+        public List<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WebAPITest/Controllers/SomeController.cs
@@ -0,0 +1,17 @@
+﻿using DataLayer.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPITest.Controllers
+{
+    public class SomeController : ApiController
+    {
+        public SomeController(IMySvcRepository r)
+        {
+        }
+    }
+}
